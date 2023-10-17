073cf8922554f46a26f9cc4ca9c221c3d2920062
Bex <bexmetzger@gmail.com>
2023-10-17 10:30:15 -0700

    Weather Map Methods

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsetting.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Enter zip:");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"The temperature is currently {WeatherMap.GetTemp(apiCall)} degrees in {WeatherMap.GetCity(apiCall)}");
+
+        }
+    }
+}
